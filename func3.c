8cf60877ccd5430302e85541cda72bbe33b88d1c
Mo2Hefny <moamensaied1@gmail.com>
2023-02-17 15:04:58 +0200

    fix

--- func3.c
+++ func3.c
@@ -33,7 +33,7 @@ void _pstr(stack_t **stack, unsigned int line_number)
   stack_t *tmp = *stack;
   while (tmp)
   {
-    if (!tmp->n || !iswascii(tmp->n))
+    if (!tmp->n || tmp->n < 0 ||  tmp->n > 127)
       break;
     printf("%c", tmp->n);
     tmp = tmp->next;
