ddf59a4bacf30576452cbbc59c7e7b5d5666c564
Mo2Hefny <moamensaied1@gmail.com>
2023-02-17 00:59:41 -0800

    main

---
+++ monty.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * main - LIFO, FIFO program
+ * @ac: number of arguments
+ * @av: arguments string array
+ * Return: 0 on success
+ */
+
+int main(int ac, char** av)
+{
+if (ac != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+return (EXIT_FAILURE);
+}
+}
---
+++ monty.h
@@ -0,0 +1,10 @@
+#ifndef MONTY
+#define MONTY
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
